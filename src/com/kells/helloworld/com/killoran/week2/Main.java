f5bea4980b208660ca8ae781b3aa9687f0459134
James Kells <james.kells@rocketmail.com>
2018-01-23 13:13:07 -0500

    Hi

---
+++ src/com/kells/helloworld/com/killoran/week2/Main.java
@@ -0,0 +1,15 @@
+package com.kells.helloworld.com.killoran.week2;
+
+/**
+ * Created by 004159 on 1/23/2018.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Welcome to week2!");
+    }
+}
+
+
